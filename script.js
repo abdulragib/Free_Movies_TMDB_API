3cffbd0949bb2a9bb5313396410d32ac77cb8be9
Abdul Ragib <abdulragib36@gmail.com>
2022-12-26 22:37:31 +0530

    header + body done

---
+++ script.js
@@ -0,0 +1,79 @@
+const base_url = 'https://api.themoviedb.org/3'
+const end_point='/discover/movie?sort_by=popularity.desc'
+const api_key='&api_key=89bd96090b6c1c5f41e3ba71b98b9548'
+
+const api_url=base_url+end_point+api_key
+
+const Movies = async (url) => {
+ const movie = await fetch(url)
+ const movie_data = await movie.json()
+ const data = await movie_data["results"]
+ console.log(data)
+ return data
+}
+
+const data = Movies(api_url)
+showPopularMovie(data)
+
+const base_img='https://image.tmdb.org/t/p/w500'
+
+const main = document.getElementById('main')
+
+function showPopularMovie(HightRatedMovie){
+    console.log(HightRatedMovie)
+
+    //HightRatedMovie is promise that why we use .then
+    HightRatedMovie.then(
+       (movie) => {
+        movie.forEach((movie) => {
+            const movieDiv = document.createElement('div')
+            setAttributes(movieDiv, {"class": "card col-md-2 border border-dark slider pt-3 pb-3 hover-box"});
+            
+            movieDiv.innerHTML=`
+            <div class="movie">
+                <img src="${base_img+movie['poster_path']}" alt="${movie['original_title']}" width="175px" height="230px">
+                <p>${movie['original_title']}</p>
+            </div>
+            `
+            console.log(movieDiv)
+            main.appendChild(movieDiv)
+        })
+       } 
+    )
+}
+
+function setAttributes(card, attrs) {
+    for(var key in attrs) {
+      card.setAttribute(key, attrs[key]);
+    }
+  }
+
+
+//Hight Rated Movies
+const mainOne = document.getElementById('mainOne')
+const end_point_url = '/discover/movie/?certification_country=US&certification=R&sort_by=vote_average.desc'
+const dataOne = Movies(base_url+end_point_url+api_key) 
+showHightRatedMovie(dataOne)
+
+function showHightRatedMovie(HightRatedMovie){
+    console.log(HightRatedMovie)
+
+    //HightRatedMovie is promise that why we use .then
+    HightRatedMovie.then(
+       (movie) => {
+        movie.forEach((movie) => {
+            const movieDiv = document.createElement('div')
+            setAttributes(movieDiv, {"class": "card col-md-2 border border-dark slider pt-3 pb-3 hover-box"});
+            
+            movieDiv.innerHTML=`
+            <div class="movie">
+                <img src="${base_img+movie['poster_path']}" alt="${movie['original_title']}" width="175px" height="230px">
+                <p>${movie['original_title']}</p>
+            </div>
+            `
+            console.log(movieDiv)
+            mainOne.appendChild(movieDiv)
+        })
+       } 
+    )
+}
